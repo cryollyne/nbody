b0b94b7494ebbcfaf9d16bd4b459b03bb36db593
Patrick <63032462+cryollyne@users.noreply.github.com>
2024-04-10 23:42:30 -0400

    added windows support for data directory

--- src/backend.cpp
+++ src/backend.cpp
@@ -6,8 +6,14 @@ Backend::Backend(QObject *parent)
     : QObject(parent)
 {
     QDir dir = QDir(QCoreApplication::applicationDirPath());
+#ifdef __linux__
     dir.cdUp();
     dir.cd("share");
     dir.cd("nbody");
+#endif
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) || defined(__WIN32__) && !defined(__CYGWIN__)
+    dir.cd("data");
+    dir.cd("nbody");
+#endif
     Backend::DATADIR = dir;
 }
